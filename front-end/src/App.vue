2f01bdf9e781aeb9ee6bf7a7e5ba6ab3963d0409
Kinsley Merrill <kinsleychristensen@Kinsleys-MacBook-Pro.local>
2020-04-12 13:45:27 -0600

    first commit

---
+++ front-end/src/App.vue
@@ -0,0 +1,84 @@
+<template>
+<div id="app">
+  <div class="header">
+    <router-link to="/">
+      <div class="logo">
+        <img src="/ordinary.png">
+      </div>
+    </router-link>
+    <div class="title">
+      <h1>Museum of Ordinary Objects</h1>
+    </div>
+  </div>
+  <div class="content">
+    <router-view />
+  </div>
+  <div class="h-divider"></div>
+  <div class="footer">
+    <div class="a-link"><router-link to="/admin">Admin</router-link></div>
+    <div class="github"><p>Find Kinsley on <a href="https://github.com/BYU-CS-260-Winter-2020/lab-4-museum-of-ordinary-objects-kinsleyc" target="_blank">Github</a></p></div>
+  </div>
+</div>
+</template>
+
+<style>
+html {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'Montserrat', sans-serif;
+  font-size: 16px;
+  background: #fff;
+  padding: 0px;
+  margin: 0px;
+}
+
+/* Header */
+.header {
+  display: flex;
+  padding: 10px 100px 0px 100px;
+  background-color: #5BDEFF;
+  color: #1C454F;
+}
+
+.title {
+  margin-top: 5px;
+}
+
+.title h1 {
+  font-size: 30px;
+}
+
+.content {
+  padding: 20px 100px;
+  min-height: 500px;
+}
+
+/* Footer */
+.footer {
+  display: flex;
+  flex-direction: column;
+  height: 50px;
+  padding: 20px 100px 0px 100px;
+  background: #e3e3e3;
+  font-size: 12px;
+  align-items: center;
+}
+
+.footer a {
+  color: #000;
+}
+
+.footer a:hover {
+  text-decoration: none;
+}
+
+h1 {
+  font-size: 20px;
+}
+
+h2 {
+  font-size: 14px;
+}
+</style>

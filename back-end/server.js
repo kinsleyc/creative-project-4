49e6b611b8776e9bf168ab34ee5565dabe211106
Kinsley Merrill <kinsley.christensen97@gmail.com>
2020-04-13 00:54:51 +0000

    digital ocean commit

--- back-end/server.js
+++ back-end/server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({
 const mongoose = require('mongoose');
 
 // connect to the database
-mongoose.connect('mongodb://localhost:27017/museum', {
+mongoose.connect('mongodb://localhost:27017/blog', {
   useNewUrlParser: true
 });
 
@@ -18,13 +18,12 @@ mongoose.connect('mongodb://localhost:27017/museum', {
 // Configure multer so that it will upload to '../front-end/public/images'
 const multer = require('multer')
 const upload = multer({
-  dest: '../front-end/public/images/',
+  dest: '/var/www/cp4.kinsleymerrill.com/images/',
   limits: {
     fileSize: 10000000
   }
 });
 
-// Create a scheme for items in the museum: a title and a path to an image.
 const itemSchema = new mongoose.Schema({
   title: String,
   path: String,
@@ -32,11 +31,8 @@ const itemSchema = new mongoose.Schema({
   date: String,
 });
 
-// Create a model for items in the museum.
 const Item = mongoose.model('Item', itemSchema);
 
-// Upload a photo. Uses the multer middleware for the upload and then returns
-// the path where the photo is stored in the file system.
 app.post('/api/photos', upload.single('photo'), async (req, res) => {
   // Just a safety check
   if (!req.file) {
@@ -47,7 +43,6 @@ app.post('/api/photos', upload.single('photo'), async (req, res) => {
   });
 });
 
-// Create a new item in the museum: takes a title and a path to an image.
 app.post('/api/items', async (req, res) => {
   const item = new Item({
     title: req.body.title,
@@ -64,7 +59,6 @@ app.post('/api/items', async (req, res) => {
   }
 });
 
-// Get a list of all of the items in the museum.
 app.get('/api/items', async (req, res) => {
   try {
     let items = await Item.find();
@@ -103,4 +97,4 @@ app.put('/api/items/:id', async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log('Server listening on port 3000!'));
+app.listen(3001, () => console.log('Server listening on port 3001!'));
